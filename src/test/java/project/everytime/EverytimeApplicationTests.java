9dbd45f99b9f534fd17c94383d5ee6ea36d1ed63
chaos0103 <lyt1228@naver.com>
2022-09-26 00:09:20 +0900

    Initial commit

---
+++ src/test/java/project/everytime/EverytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package project.everytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
