54bf9539c74ca5c2b10a814e62f8038636cb16ab
chaos0103 <lyt1228@naver.com>
2022-09-30 16:33:00 +0900

    feat: 로그인 세션 체크 기능 구현

---
+++ src/main/java/project/everytime/WebConfig.java
@@ -0,0 +1,27 @@
+package project.everytime;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import project.everytime.login.LoginArgumentResolver;
+import project.everytime.login.LoginCheckInterceptor;
+
+import java.util.List;
+
+//@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns();
+    }
+}
