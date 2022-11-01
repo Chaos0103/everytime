ab5d9c684a5c39ea7848d4bc4e697e0a42ed8c16
chaos0103 <lyt1228@naver.com>
2022-11-01 17:04:42 +0900

    refactor: 변수 변경

--- src/main/java/project/everytime/client/user/dto/LoginUser.java
+++ src/main/java/project/everytime/client/user/dto/LoginUser.java
@@ -1,7 +1,6 @@
 package project.everytime.client.user.dto;
 
 import lombok.Data;
-import project.everytime.client.UploadFile;
 import project.everytime.client.user.AccountStatus;
 import project.everytime.client.user.AuthType;
 import project.everytime.client.user.User;
@@ -11,15 +10,16 @@ public class LoginUser {
 
     private Long id;
     private Long schoolId;
+    private String schoolName;
     private String loginId;
     private String username;
     private String birth;
     private String phone;
     private String nickname;
-    private String yearOfAdmission;
+    private String enterYear;
     private String sex;
     private String email;
-    private UploadFile uploadFile;
+    private String storeFileName;
     private AuthType authType;
     private boolean agreementAd;
     private AccountStatus status;
@@ -27,15 +27,16 @@ public class LoginUser {
     public LoginUser(User user) {
         this.id = user.getId();
         this.schoolId = user.getSchool().getId();
+        this.schoolName = user.getSchool().getName().replaceAll("대학교", "대");
         this.loginId = user.getLoginId();
         this.username = user.getUsername();
         this.birth = user.getBirth();
         this.phone = user.getPhone();
         this.nickname = user.getNickname().getNickname();
-        this.yearOfAdmission = user.getEnterYear();
+        this.enterYear = user.getEnterYear();
         this.sex = user.getSex();
         this.email = user.getEmail();
-        this.uploadFile = user.getUploadFile();
+        this.storeFileName = user.getUploadFile().getStoreFileName();
         this.authType = user.getAuthType();
         this.agreementAd = user.isAgreementAd();
         this.status = user.getStatus();
